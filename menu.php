81653ae31513f9054be6c69d741e04d8c0fd878a
ch <carlosbv1@gmail.com>
2023-06-05 15:47:43 -0300

    nessa versao eu fiz tal coisa

---
+++ menu.php
@@ -0,0 +1,11 @@
+ <nav>
+    <div class="nav-wrapper blue lighten-2">
+      <a href="#" class="brand-logo right">Oficina CH</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="cad_cliente.php">Cad Cliente</a></li>
+        <li><a href="cad_os.php">Cad OS</a></li>
+        <li><a href="pesq_os.php">Pes OS</a></li>
+      </ul>
+    </div>
+  </nav>
+        
